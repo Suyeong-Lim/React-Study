2bca2c3567b5db7366ae6a333039371708eeed1f
SY <oowwo@naver.com>
2022-05-17 16:15:39 +0900

    Add: practice 03

---
+++ practice03/src/App.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLogin = localStorage.getItem("isLoggedIn");
+    if (checkLogin === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  return (
+    <React.Fragment>
+      <AuthContext.Provider
+        value={{ isLoggedIn: isLoggedIn, onLogout: logoutHandler }}
+      >
+        <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
+    </React.Fragment>
+  );
+}
+
+export default App;
